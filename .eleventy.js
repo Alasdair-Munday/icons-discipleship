2466a36d7ecf6830631a5036b3230beededf37ec
Alasdair Munday <alasdair.munday@sky.uk>
2021-03-11 11:57:14 +0000

    added sketchbook font

--- .eleventy.js
+++ .eleventy.js
@@ -41,6 +41,8 @@ module.exports = function (eleventyConfig) {
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
 
+  eleventyConfig.addPassthroughCopy("./src/static/fonts");
+
   // Copy favicon to route of /_site
   eleventyConfig.addPassthroughCopy("./src/favicon.ico");
 
